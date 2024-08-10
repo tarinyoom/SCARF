9a0b3cd66e7c02ee91986cffe52fc15e297ea5e4
Adam Reynolds <36433367+tarinyoom@users.noreply.github.com>
2024-08-10 10:47:02 -0700

    Remove unused animation

--- src/cli.cpp
+++ src/cli.cpp
@@ -3,25 +3,13 @@
 #include <span>
 #include <functional>
 
-#include "black_box.hpp"
 #include "three_body.hpp"
 #include "mov.hpp"
 
 auto run_cli(std::ostream& out, std::vector<std::string>& args) -> int {
-	if (args.size() <= 1) {
-		return -1;
+	if (args.size() == 0 || args[0] == "three_body") {
+		return make_mov(three_body);
+	} else {
+		return 0;
 	}
-
-	auto mode = args[0];
-	auto anim = args[1];
-    if (mode == "mov") {
-		if (anim == "black_box") {
-			return make_mov(black_box);
-		} else {
-			return make_mov(three_body);
-		}
-	} else if (mode == "gif") {
-		throw "NYI";
-	}
-	return 0;
 }
