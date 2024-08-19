0e248cbea5d8189289ec0c50daed6858359eb2b2
Adam Reynolds <36433367+tarinyoom@users.noreply.github.com>
2024-08-19 00:17:51 -0700

    Remove CLI

--- src/cli.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include <ostream>
-#include <string>
-#include <vector>
-
-auto run_cli(std::ostream& out, std::vector<std::string>& args) -> int;
--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,6 @@
-﻿#include <string>
-#include <vector>
-#include <iostream>
-
-#include "cli.hpp"
+﻿#include "mov.hpp"
+#include "sph.hpp"
 
 auto main(int argc, char* argv[]) -> int {
-    std::vector<std::string> args;
-    for (int i = 1; i < argc; i++) {
-        args.push_back(std::string(argv[i]));
-    }
-    run_cli(std::cout, args);
+	return make_mov(sph_animation);
 }
