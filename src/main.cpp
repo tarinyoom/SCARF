f94827a4dd87821c88ebc76107a318c18836d8e2
Adam Reynolds <36433367+tarinyoom@users.noreply.github.com>
2024-08-25 12:25:47 -0700

    Separate out lib and driver

--- src/main.cpp
+++
@@ -1,4 +0,0 @@
-﻿#include "mov.hpp"
-#include "sph.hpp"
-
-auto main(int argc, char* argv[]) -> int { return make_mov(sph_animation); }
---
+++ tools/driver.cpp
@@ -0,0 +1,4 @@
+#include "mov.hpp"
+#include "sph.hpp"
+
+auto main() -> int { return make_mov(sph_animation); }
