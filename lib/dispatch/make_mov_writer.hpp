c1ad43d67035bfce96a2d919a29ab70b7f690170
Adam Reynolds <adam.chiu.reynolds@gmail.com>
2024-12-14 10:48:56 -0800

    Remove bad implementation of mov writer

--- lib/dispatch/make_mov_writer.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <memory>
-#include <string>
-
-#include "IVideoWriter.hpp"
-
-auto make_mov_writer(std::string_view filepath, int height, int width, int fps)
-    -> std::unique_ptr<IVideoWriter>;
