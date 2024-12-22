6954176b912008baa3d952d606a4bc571f01460a
Adam Reynolds <adam.chiu.reynolds@gmail.com>
2024-12-22 11:26:13 -0800

    Add benchmarking

--- lib/sph.cpp
+++ lib/sph.cpp
@@ -1,5 +1,6 @@
 #include "sph.hpp"
 
+#include <chrono>
 #include <glm/glm.hpp>
 #include <iostream>
 #include <memory>
@@ -37,6 +38,10 @@ auto render_state(const model::State& state) -> Grid<Pixel> {
 }
 
 auto run(int argc, char* argv[]) -> int {
+  using Clock = std::chrono::high_resolution_clock;
+
+  auto start = Clock::now();
+
   auto writer = dispatch::make_video_writer("mov", "examples/generated.mov");
   int i = 0;
   Metrics m;
@@ -45,15 +50,23 @@ auto run(int argc, char* argv[]) -> int {
 
   using Callback = std::function<void(const model::State& state)>;
 
+  std::chrono::milliseconds render_duration(0);
+
   Callback start_callback = [&](const auto&) { m["n_steps"]++; };
 
   Callback render_callback = [&](const auto& state) {
+    auto start = Clock::now();
+
     if (i++ % 10 == 0) {
       auto image = render_state(state);
       writer->write_frame(image);
       m["n_frames"]++;
       std::cout << "Generating frame " << i / 10 + 1 << std::endl;
     }
+    auto end = Clock::now();
+
+    render_duration +=
+        std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
   };
 
   Engine<model::State> engine{.step = model::step,
@@ -62,7 +75,11 @@ auto run(int argc, char* argv[]) -> int {
   auto initial_state = model::init();
 
   run_engine<model::State>(engine, 3000, 0.001, std::move(initial_state));
-
+  auto end = Clock::now();
+  auto duration =
+      std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
+  m["total_duration"] = duration.count();
+  m["render_duration"] = render_duration.count();
   print_metrics(m);
   return 0;
 }
